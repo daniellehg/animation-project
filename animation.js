895a8c44bb238fe9afc71ca74c75874281adcc3b
Danielle H Gamble <dg12181@srcs.loc>
2018-05-25 13:49:46 -0700

    added interactionn

--- animation.js
+++ animation.js
@@ -4,6 +4,16 @@ var xpos = 0;
 var ypos = 0;
 var xstep = 1;
 var ystep = 1;
+
+container.addEventListener("click", moveGiorvi);
+
+function moveGiorvi(event) {
+    var xPosition = event.clientX;
+	var yPosition = event.clientY;
+	grouch.style.left = xPosition + "px";
+	grouch.style.top = yPosition + "px";
+}
+
 var id = setInterval(frame, 5);
 function frame() {
     if (xpos > 400 || xpos < 0) {
@@ -17,6 +27,6 @@ function frame() {
     ypos = ypos + ystep;
     box.style.top = ypos + 'px';
     box.style.left = xpos + 'px';
-     grouch.style.top = (ypos + 700) + 'px';
-   grouch.style.left = xpos + 'px';
+
+
 }
